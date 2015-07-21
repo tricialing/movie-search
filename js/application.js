f7ffd3a9123ca3a2e6b8876c217105b8b249e0c3
Tricia Ling <tricia@mydiylab.com>
2015-07-22 01:43:35 +0800

    added: search function & connected to API

--- js/application.js
+++ js/application.js
@@ -1,4 +1,35 @@
 $(document).ready(function(){
   console.log("linked");
+
+  var searchStart = function(title){
+
+    $.ajax({
+      type: "GET", 
+      url: 'http://www.omdbapi.com/?t=' + title,
+      dataType: "JSON",
+      success: function(response){
+        console.log(response);
+      }
+    });
+  };
+
+  $('#search-form').submit(function(){
+    event.preventDefault();
+    searchStart($('#search-box').val());
+    // console.log($('#search-box').val());
+  });
+
 });
+
+
+  // var successFunction = function(response){
+  //   console.log("yes");
+  // };
+
+   // var keys = ['Title', 'Year'];
+  // $('#search-form').submit(function(){
+  //   event.preventDefault();
+  // });
+
+
 
