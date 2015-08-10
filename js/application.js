aae4c7e2a7574cac0179bf1a5eb0631c9d3652c1
Tricia Ling <tricia@mydiylab.com>
2015-08-10 23:20:39 +0800

    feat(search): styling

--- js/application.js
+++ js/application.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     var successFunction = function(response){
         console.log(response);
         $("img").attr("src", response.Poster);
-        $(".details").text(response.Title);
+        $(".details").html('<h2>' + response.Title + '</h2><h3>Released Date: </h3> <p>' + response.Released + '</p> <h3>Actors: </h3> <p>'+ response.Actors + '</p> <h3>Plot: </h3> <p>' + response.Plot + '</p> <h3>Runtime: </h3> <p>' + response.Runtime + '</p>');
         // Get poster
         // var poster = function(response){
         //   $("img").attr("src", response.Poster);
