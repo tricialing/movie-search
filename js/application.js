72d646e6a0c90b37867932d71b7b8b8eeb8ec19a
Tricia Ling <tricia@mydiylab.com>
2015-08-10 10:52:07 +0800

    add(feat): search + return with info

--- js/application.js
+++ js/application.js
@@ -1,35 +1,39 @@
 $(document).ready(function(){
   console.log("linked");
 
-  var searchStart = function(title){
-
-    $.ajax({
-      type: "GET", 
-      url: 'http://www.omdbapi.com/?t=' + title,
-      dataType: "JSON",
-      success: function(response){
-        console.log(response);
-      }
-    });
-  };
-
+  // hit enter to search
   $('#search-form').submit(function(){
     event.preventDefault();
-    searchStart($('#search-box').val());
-    // console.log($('#search-box').val());
+
+    var title = $('#search-box').val();
+    searchStart(title);
+    console.log("title: "+ title);
   });
 
+  var searchStart = function(title){
+
+    var successFunction = function(response){
+        console.log(response);
+    }
+
+    $.ajax({
+      type: "GET",
+      url: "http://www.omdbapi.com/?t=" + title,
+      dataType: "JSON",
+      success: successFunction
+    });
+  }; //end of searchStart
+
+
+
+
+    //Get poster
+    // var poster = function(response){
+    //   $("img").attr("src", response.Poster);
+    //   console.log("poster");
+    // };
+
+    //Get details
+    // var keys = Object.keys(response);
+
 });
-
-
-  // var successFunction = function(response){
-  //   console.log("yes");
-  // };
-
-   // var keys = ['Title', 'Year'];
-  // $('#search-form').submit(function(){
-  //   event.preventDefault();
-  // });
-
-
-
